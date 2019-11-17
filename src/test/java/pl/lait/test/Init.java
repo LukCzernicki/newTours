86565adbeb2f1e476972ee9cae7e68b2aab2808b
Łukasz <lukczernicki@gmail.com>
2019-11-17 10:05:41 +0100

    pierwszy etap rezerwacji lotu

--- src/test/java/pl/lait/test/Init.java
+++ src/test/java/pl/lait/test/Init.java
@@ -1,5 +1,4 @@
 package pl.lait.test;
-import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -14,7 +13,7 @@ public class Init {
         System.out.println("Odpalamy Driver");
         driver = new ChromeDriver();
         driver.get("http://newtours.demoaut.com/");
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); 
+       
 		
 		return driver;
 	}
--- src/test/java/pl/lait/test/ReservationTest.java
+++ src/test/java/pl/lait/test/ReservationTest.java
@@ -5,45 +5,75 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 public class ReservationTest {
-
 	WebDriver driver;
 
 	@Before
 	public void openPage() {
 		driver = Init.getDriver();
+
 	}
 
 	@Test
-	public void topMenuTest() {
-		System.out.println(" !!wewnątrz testu!!");
+	public void reserveFlight() {
 		driver.findElement(By.linkText("SIGN-ON")).click();
-		driver.findElement(By.linkText("REGISTER")).click();
-		driver.findElement(By.linkText("SUPPORT")).click();
-		driver.findElement(By.linkText("CONTACT")).click();
-		String title = driver.getTitle();
-		System.out.println(title);
+		driver.findElement(By.name("userName")).sendKeys("Heniek");
+		driver.findElement(By.name("password")).sendKeys("1qa2ws#ed");
+		driver.findElement(By.name("login")).click();
+
+		String oneWayRadioXpath = "/html/body/div/table/tbody/tr/td[2]/table/tbody/tr[4]"
+				+ "/td/table/tbody/tr/td[2]/table/tbody/tr[5]" 
+				+ "/td/form/table/tbody/tr[2]/td[2]/b/font/input[2]";
+
+		driver.findElement(By.xpath(oneWayRadioXpath)).click();
+		
+		WebElement fromPort = driver.findElement(By.name("fromPort"));
+		Select fromPortSelect = new Select(fromPort);
+		fromPortSelect.selectByVisibleText("London");
+		
+		WebElement passCount = driver.findElement(By.name("passCount"));
+		Select passCountSelect = new Select(passCount);
+		passCountSelect.selectByValue("2");
+		
+		WebElement fromDay = driver.findElement(By.name("fromDay"));
+		Select fromDaySelect = new Select(fromDay);
+		fromDaySelect.selectByValue("20");
+		
+		WebElement fromMonth = driver.findElement(By.name("fromMonth"));
+		Select fromMonthSelect = new Select(fromMonth);
+		fromMonthSelect.selectByValue("12");
+		
+		WebElement toDay = driver.findElement(By.name("toDay"));
+		Select toDaySelect = new Select(toDay);
+		toDaySelect.selectByValue("24");
+		
+		WebElement toMonth = driver.findElement(By.name("toMonth"));
+		Select toMonthSelect = new Select(toMonth);
+		toMonthSelect.selectByValue("12");
+		
+		String firstClass = "/html/body/div/table/tbody/tr/td[2]/table/"
+				+ "tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/"
+				+ "td/form/table/tbody/tr[9]/td[2]/font/font/input[2]";
+		
+		driver.findElement(By.xpath(firstClass)).click();
+		
+		WebElement airline = driver.findElement(By.name("airline"));
+		Select airlineSelect = new Select(airline);
+		airlineSelect.selectByVisibleText("Unified Airlines");
+		
+		driver.findElement(By.name("findFlights")).click();
+		
+		
+		
+		
 	}
 
-	@Test
-	public void leftMenuTest() {
-		System.out.println(" !!wewnątrz testu lewego menu!!");
-		driver.findElement(By.linkText("Home")).click();
-		driver.findElement(By.linkText("Flights")).click();
-		driver.findElement(By.linkText("Hotels")).click();
-		driver.findElement(By.linkText("Car Rentals")).click();
-		driver.findElement(By.linkText("Cruises")).click();
-		driver.findElement(By.linkText("Destinations")).click();
-		driver.findElement(By.linkText("Vacations")).click();
-		String title = driver.getTitle();
-		System.out.println(title);
-	}
-	
 	@After
 	public void tearDown() {
-		driver.close();
+		//driver.close();
 	}
-	
 
 }
